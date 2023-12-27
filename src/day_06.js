15f481a710b0b0e7f1a0a3af443f925ec5a0d045
Shawn Salat <31082405+ThinkSalat@users.noreply.github.com>
2023-12-27 01:25:07 -0600

    finish day 6

--- src/day_06.js
+++ src/day_06.js
@@ -3,6 +3,21 @@
  * Puzzle Description: https://adventofcode.com/2023/day/6
  */
 
+const getNumSolutions = (time, distance) => {
+  const discriminant = time * time - 4 * distance;
+
+  const sqrtDiscriminant = Math.sqrt(discriminant);
+
+  // Calculate the floor and ceiling values
+  const floorValue = Math.floor((-time + sqrtDiscriminant) / -2);
+  const ceilValue = Math.ceil((-time - sqrtDiscriminant) / -2);
+
+  // Calculate the number of solutions within the valid range
+  const numSolutions = Math.min(floorValue, time - 1) - ceilValue + 1
+
+  return Math.abs(numSolutions);
+}
+
 /**
  * Returns the solution for level one of this puzzle.
  * @param {Object} args - Provides both raw and split input.
@@ -11,7 +26,16 @@
  * @returns {Number|String}
  */
 export const levelOne = ({ input, lines }) => {
-  // your code here
+  // lines = ['Time:      7  15   30', 'Distance:  9  40  200']
+  const times = lines[0].match(/\d+/g)
+  const distance = lines[1].match(/\d+/g)
+
+  let product = 1
+  times.forEach((time, i) => {
+    product *= getNumSolutions(time, distance[i])
+  })
+
+  return product
 };
 
 /**
@@ -22,5 +46,8 @@ export const levelOne = ({ input, lines }) => {
  * @returns {Number|String}
  */
 export const levelTwo = ({ input, lines }) => {
-  // your code here
+  const time = +lines[0].replaceAll(' ', '').split(':')[1]
+  const distance = +lines[1].replaceAll(' ', '').split(':')[1]
+
+  return getNumSolutions(time, distance)
 };
